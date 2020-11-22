f6d303abb57fa856ebb7d3bad67af1cce66a31a5
Taddeus <taddeus_b90@hotmail.com>
2020-11-22 21:27:41 +0200

    styled view session

--- src/app/pages/view-session/view-session.component.ts
+++ src/app/pages/view-session/view-session.component.ts
@@ -9,16 +9,23 @@ import { DataStoreService } from '../../services/data-store/data-store.service';
 })
 export class ViewSessionComponent implements OnInit {
   public sessionId: number;
-  public sessionInfo: any[];
-  public metrics: number[];
+  public sessionInfo: any[] = [];
+  public metrics: number[] = [];
+  public deltas: number[] = [];
+  public displayedColumns: string[] = ['batch', 'rn', 'average_metric', 'delta'];
+
   constructor(private dataStoreService: DataStoreService, private activatedRoute: ActivatedRoute) {
     this.sessionId = Number(activatedRoute.snapshot.paramMap.get('id'));
   }
 
   ngOnInit(): void {
     this.dataStoreService.getAggregatedSessionInfo(this.sessionId).then((data) => {
-      this.sessionInfo = data;
+      this.sessionInfo = data.map((item, index, arr) => ({
+        ...item,
+        delta: index === 0 ? 0 : arr[index].average_metric - arr[index - 1].average_metric,
+      }));
       this.metrics = data.map((item) => item.average_metric);
+      this.deltas = this.sessionInfo.map((item) => item.delta);
     });
   }
 }
